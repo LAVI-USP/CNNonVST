106f1662bed88059a00b2b9088cde2971721abf8
rodrigovimieiro <rodrigo.vimieiro@gmail.com>
2023-06-01 13:26:19 -0300

    new weights

--- bash_tune_rnw.py
+++ bash_tune_rnw.py
@@ -8,47 +8,84 @@ Created on Wed Mar 30 15:28:51 2022
 
 import os
 
-python_path = "/home/rodrigo/.virtualenvs/science/bin/python"
+from libs.utilities import removedir, makedir
 
-reduc_factors = [50]
-rnw_weights = ['0.01357164'] #['0.1357164','3.3800761', '9.4957105'] # ['']#
+python_path = "/home/rodrigo/Documents/Rodrigo/Python_VE/science/bin/python"
+# python_path = "/home/laviusp/Documents/pyvenv/science/bin/python"
 
-model_type = 'VSTasLayer-MNSE_rnw' #'Noise2Sim'#
+restoration_path = '/media/rodrigo/Dados_2TB/Imagens/UPenn/Phantom/Anthropomorphic/DBT/Restorations/31_30/'
+# restoration_path = '/home/laviusp/Documents/Rodrigo_Vimieiro/phantom/Restorations/31_30'
 
-## now loop through the above array
-for reduc_factor in reduc_factors:
+rnw_weights = [0.00000000, 0.10526316, 0.21052632, 0.31578947, 0.42105263,
+               0.52631579, 0.63157895, 0.73684211, 0.84210526, 0.94736842,
+               1.05263158, 1.15789474, 1.26315789, 1.36842105, 1.47368421,
+               1.57894737, 1.68421053, 1.78947368, 1.89473684, 2.00000000,
+               0.00501253, 0.01002506, 0.01503759, 0.02005013, 0.02506266,
+               0.03007519, 0.03508772, 0.04010025, 0.04511278, 0.05012531,
+               0.05513785, 0.06015038, 0.06516291, 0.07017544, 0.07518797,
+               0.08020050, 0.08521303, 0.09022557, 0.09523810, 0.10025063]
+
+# rnw_weights = [0.0031493]
+
+models = ['ResResNet', 'UNet2', 'RED']
+
+# models = ['RED']
+
+framework = 'Noise2Sim'#'VSTasLayer-MNSE'  #
+n_epochs = 5
+
+for model in models:
     for rnw_weight in rnw_weights:
-                
-        # ##########  Train ##########
+
+        ##########  Train ##########
         python_command = "{} main_training_MNSE.py \
-                  --rf {} \
-                  --rfton {} \
-                  --rnw {}".format(python_path, reduc_factor, 50, rnw_weight)
+                  --nep {} \
+                  --rnw {} \
+                  --model {}".format(python_path,
+                                     n_epochs,
+                                     rnw_weight,
+                                     model)
 
         print(python_command)
         os.system(python_command)
-        
-        ##########  Test ##########
+
+        #########  Test ##########
         python_command = "{} main_testing.py \
-                  --rf {} \
-                  --rfton {} \
-                  --model {}{}".format(python_path, reduc_factor, 50, model_type, rnw_weight)
+                          --nep {} \
+                          --rnw {} \
+                          --model {} \
+                          --fmw {}".format(python_path,
+                                             n_epochs,
+                                             rnw_weight,
+                                             model,
+                                             framework)
 
         print(python_command)
+        makedir(restoration_path)
         os.system(python_command)
-        
+
         ##########  MNSE ##########
-        
         python_command = "{} evaluation/MNSE.py \
-                  --rf {} \
-                  --model {}{}".format(python_path, reduc_factor, model_type, rnw_weight)
+                  --model {}_DBT_{}_rnw{} --rf {:d}".format(python_path,
+                                                            model,
+                                                            framework,
+                                                            rnw_weight,
+                                                            50)
 
         print(python_command)
         os.system(python_command)
-        
-        
-        
-        
-        
-    
 
+        ##########  SNR ##########
+        python_command = "{} evaluation/SIIM_QILV_SNR.py \
+                          --model {}_DBT_{}_rnw{} --rf {:d}".format(python_path,
+                                                                    model,
+                                                                    framework,
+                                                                    rnw_weight,
+                                                                    50)
+
+        print(python_command)
+        os.system(python_command)
+
+
+
+        # removedir(restoration_path)
